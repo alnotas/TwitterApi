52266ea9515ca71737c130158d695e999589a268
Alexander Notas <alnotas@gmail.com>
2018-06-22 16:59:58 +0300

    .

--- Twitter_Api/Form1.cs
+++ Twitter_Api/Form1.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+﻿using System.Net;
+using System.Net.Http;
 using System.Windows.Forms;
 
 namespace Twitter_Api
@@ -14,6 +9,16 @@ namespace Twitter_Api
       public Form1()
       {
          InitializeComponent();
+      }
+
+      private void button1_Click(object sender, System.EventArgs e)
+      {
+         string baseURL = "http://services3.epsilonnet.gr/es3/es/update/234-10013-197/17.2.2.0/4491978/__PKhnMkATnP_S_RsoCWBK2kqkKSaXbsfvc1zw6g2Hbua_P_mH0XQ8Sxd_P_oA_E__E";
+         HttpWebRequest request = (HttpWebRequest)WebRequest.Create(baseURL);
+         request.Method = "GET";
+         request.ContentType = @"application/json";
+         HttpResponseMessage response = (HttpResponseMessage)request.GetResponse();
+         textBox3.Text = response.Content.ToString();
       }
    }
 }
